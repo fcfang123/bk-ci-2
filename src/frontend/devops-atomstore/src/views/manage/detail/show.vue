403abe5be0c840ba7faf0605bf72b9aed079a43a
v_yjjiaoyu <1981190393@qq.com>
2026-03-11 14:42:58 +0800

    --story=131369666 应用商店支持 AI 应用

--- src/frontend/devops-atomstore/src/views/manage/detail/show.vue
+++ src/frontend/devops-atomstore/src/views/manage/detail/show.vue
@@ -25,14 +25,14 @@
     import imageShow from '@/components/manage/detail/image-detail/show.vue'
     import serviceShow from '@/components/manage/detail/service-detail/show.vue'
     import { mapGetters } from 'vuex'
-    import DEVXShow from '@/components/manage/detail/client-detail/show.vue'
+    import devxShow from '@/components/manage/detail/client-detail/show.vue'
 
     export default {
         components: {
             atomShow,
             imageShow,
             serviceShow,
-            DEVXShow
+            devxShow
         },
 
         computed: {
