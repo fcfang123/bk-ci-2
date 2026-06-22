49fb93292cde5fb179fe3ede5aa7978b9ad97d00
yjieliang <v_yjieliang@tencent.com>
2026-06-22 11:39:17 +0800

    解决冲突

--- src/backend/ci/core/common/common-pipeline/src/main/kotlin/com/tencent/devops/common/pipeline/pojo/BuildFormProperty.kt
+++ src/backend/ci/core/common/common-pipeline/src/main/kotlin/com/tencent/devops/common/pipeline/pojo/BuildFormProperty.kt
@@ -111,8 +111,7 @@ data class BuildFormProperty(
     )
     var asInstanceInput: Boolean? = null,
     @get:Schema(title = "复杂参数列表[仅type==CUSTOM_PARAM有效]", required = false)
-    var fields: List<BuildFormProperty>? = null
-    var asInstanceInput: Boolean? = null,
+    var fields: List<BuildFormProperty>? = null,
     @get:Schema(title = "所属公共变量组名称", required = false)
     var varGroupName: String? = null,
     @get:Schema(title = "所属公共变量组版本", required = false)
